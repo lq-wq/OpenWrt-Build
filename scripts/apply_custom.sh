1bd05a4027018070e73b6c35696c670b5320c4b9
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-02-15 10:33:31 +0800

    Update apply_custom.sh

--- scripts/apply_custom.sh
+++ scripts/apply_custom.sh
@@ -2,29 +2,21 @@
 
 cd openwrt
 
-# 设置主题和基础配置
-sed -i 's/CONFIG_DEFAULT_THEME=.*/CONFIG_DEFAULT_THEME=\"argon\"/' .config
-echo "CONFIG_TARGET_ROOTFS_PARTSIZE=4096" >> .config
-echo "CONFIG_TARGET_KERNEL_PARTSIZE=128" >> .config
+# 精简主题配置（保留必要文件）
+sed -i 's/CONFIG_PACKAGE_luci-theme-.*/CONFIG_PACKAGE_luci-theme-argon=y/' .config
 
-# 设置主机名和IP
-sed -i 's/CONFIG_TARGET_HOSTNAME=.*/CONFIG_TARGET_HOSTNAME=\"openwrt-NIT\"/' .config
-echo "CONFIG_IPADDR=\"192.168.6.1\"" >> .config
+# 删除冗余内核模块
+sed -i '/CONFIG_PACKAGE_kmod-/d' .config
+echo "CONFIG_PACKAGE_kmod-fs-ntfs=y" >> .config
+echo "CONFIG_PACKAGE_kmod-tcp-bbr=y" >> .config
 
-# 添加第三方源
-echo 'src-git custom https://github.com/cdny123/openwrt-package1' >> feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 限制编译尺寸
+echo "CONFIG_TARGET_IMAGES_GZIP_COMPRESS=y" >> .config
+echo "CONFIG_TARGET_ROOTFS_PARTSIZE=2048" >> .config  # 分区缩小到2GB
 
-# 克隆第三方软件包
-git clone https://github.com/sirpdboy/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/lq-wq/luci-app-quickstart.git package/luci-app-quickstart
-git clone --branch master https://github.com/vernesong/OpenClash.git package/openclash
+# 禁用调试符号
+echo "CONFIG_DEBUG=n" >> .config
+echo "CONFIG_DEBUG_INFO=n" >> .config
 
-# 签名设置
-BUILD_DATE=$(date +%Y%m%d)
-echo "CONFIG_VERSION_DIST=\"OpenWrt-04543473\"" >> .config
-echo "CONFIG_VERSION_NUMBER=\"$BUILD_DATE\"" >> .config
-
-# 生成最终配置
-make defconfig
+# 清理历史构建
+rm -rf tmp/ build_dir/ staging_dir/ logs/
