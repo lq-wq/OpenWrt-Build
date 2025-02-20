99cce4e4be26d2eac44da4e69f6dd5579e0e5783
lq-wq <61339326+lq-wq@users.noreply.github.com>
2025-02-20 08:04:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' openwrt/target/linux/x86/Makefile
 
 # 修改默认主题
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名
 sed -i 's/OpenWrt/NITT/g' package/base-files/files/bin/config_generate
@@ -45,7 +45,6 @@ echo "CONFIG_PACKAGE_luci-app-openclash-core=y" >> .config
 # 选择 X86_64 核心
 make target/linux/x86/compile
 make target/linux/x86/image/generic
-
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
 uci add dhcp domain
